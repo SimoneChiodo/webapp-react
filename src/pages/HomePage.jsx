9b1b078af55ccd82fe26d7962525e7e1781787dc
SimoneChiodo <simochi.005@gmail.com>
2025-01-13 16:47:59 +0100

    add database-fetch, movie-list in HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,26 @@
+import { useState, useEffect } from "react";
+
 export default function HomePage() {
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/")
+            .then((res) => res.json())
+            .then((data) => {
+                setMovies(data);
+                console.log(data);
+            });
+    }, []);
+
     return (
         <div className="container pt-5">
             <h1>Homepage</h1>
+
+            <ul>
+                {movies.map((movie) => (
+                    <li key={`movie-` + movie.id}>{movie.title}</li>
+                ))}
+            </ul>
         </div>
     );
 }
