1f0da8a690c09f0e07ce8d08a023633221b64288
SimoneChiodo <simochi.005@gmail.com>
2025-01-13 19:26:20 +0100

    add DetailsPage-estetic

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
+// Custom CSS
+import "../assets/DetailsPage.css";
+
 export default function DetailsPage() {
     const [movie, setMovie] = useState([]);
     const id = useParams().id;
@@ -17,21 +20,26 @@ export default function DetailsPage() {
         <div className="container pt-5">
             <h1>Detailspage</h1>
 
-            <ul>
-                <li>{movie.title}</li>
-                <li>{movie.director}</li>
-                <li>{movie.genre}</li>
-                <li>{movie.release_year}</li>
-                <li>{movie.abstract}</li>
-                <li>
+            <div className="movie-card bg-light d-flex space-between border rounded-5 p-3">
+                <div className="movie-card-left flex-grow-1 me-3">
+                    <ul className="lh-lg">
+                        <li>{movie.title}</li>
+                        <li>{movie.director}</li>
+                        <li>{movie.genre}</li>
+                        <li>{movie.release_year}</li>
+                        <li>{movie.abstract}</li>
+                        <li>{movie.created_at}</li>
+                        <li>{movie.updated_at}</li>
+                    </ul>
+                </div>
+                <div className="movie-card-right">
                     <img
+                        className="rounded-4"
                         src={"http://" + movie.image}
                         alt={`${movie.title}-cover`}
                     />
-                </li>
-                <li>{movie.created_at}</li>
-                <li>{movie.updated_at}</li>
-            </ul>
+                </div>
+            </div>
         </div>
     );
 }
